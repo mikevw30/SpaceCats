f81aa39f25c89279666e6a4fc75aa150c74a504f
Michael Van Wie <mikevw30@gmail.com>
2016-09-11 15:11:38 -0400

    gryo.js

--- assets/www/game/js/prefabs/ship.js
+++ assets/www/game/js/prefabs/ship.js
@@ -1,14 +1,23 @@
 'use strict';
 
 var Ship = function(game, x, y, frame) {  
-  Phaser.Sprite.call(this, game, x, y, 'ship', frame);
-  this.name = 'ship';
+	Phaser.Sprite.call(this, game, x, y, 'ship', frame);
+	this.name = 'ship';
   
-  this.anchor.setTo(-0.2, 0.5);
-  this.game.physics.arcade.enableBody(this);
-  this.body.gravity.y = 1000;
-  this.game.add.existing(this);
-  this.body.immovable = true;
+	this.anchor.setTo(-0.2, 0.5);
+	this.game.physics.arcade.enableBody(this);
+	//this.body.gravity.y = 1000;
+	this.game.add.existing(this);
+	this.body.immovable = true;
+  
+	gyro.frequency = 10;
+	// start gyroscope detection
+	gyro.startTracking(function(o) {
+	     // updating player velocity
+	     this.body.velocity.x += o.gamma/20;
+	     this.body.velocity.y += o.beta/20;
+	});
+  
 };
 
 Ship.prototype = Object.create(Phaser.Sprite.prototype);  
@@ -21,6 +30,6 @@ Ship.prototype.update = function() {
 };
 
 Ship.prototype.jump = function() {
-	this.body.velocity.y = -350;
+//	this.body.velocity.y = -350;
 	this.game.add.tween(this).to({angle: -20}, 100).start(); 
 };
