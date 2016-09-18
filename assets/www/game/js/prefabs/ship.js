e2ff78783c7ac061e99cf5f4b1d9bc2d2931960e
Michael Van Wie <mikevw30@gmail.com>
2016-09-18 18:33:50 -0400

    ship movement update.

--- assets/www/game/js/prefabs/ship.js
+++ assets/www/game/js/prefabs/ship.js
@@ -20,13 +20,13 @@ Ship.prototype.update = function() {
 	var gX = gyro.getOrientation().x;
 	var gY = gyro.getOrientation().y;
 
-	if(Math.abs(gX) >=1){
+	if(Math.abs(gX) >=0.5){
 		this.body.velocity.x -= gX;
 	}
 	else{
 		this.body.velocity.x = 0;
 	}
-	if(Math.abs(gY) >=1){
+	if(Math.abs(gY) >=0.5){
 		this.body.velocity.y += gY;
 	}
 	else{
