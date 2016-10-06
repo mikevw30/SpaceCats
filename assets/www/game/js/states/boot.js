e9a3bfd6da621779ce0797a545faa767d8651699
Michael Van Wie <mikevw30@gmail.com>
2016-10-05 22:04:22 -0400

    ES6 test

--- assets/www/game/js/states/boot.js
+++ assets/www/game/js/states/boot.js
@@ -1,17 +1,34 @@
-'use strict';
-function Boot(game) {}
+//'use strict';
+//function Boot(game) {}
+//
+//Boot.prototype = {
+//  init: function(){
+//	  game.scale.forceOrientation(false, true);
+//  },
+//  preload: function() {
+//      this.game.load.image("loading","game/assets/loading.png");
+//  },
+//  create: function() {
+//	  console.log("boot state");
+//	  this.game.state.start("Preload");
+//  },
+//  update: function() {
+//  }
+//};
 
-Boot.prototype = {
-  init: function(){
-	  game.scale.forceOrientation(false, true);
-  },
-  preload: function() {
-      this.game.load.image("loading","game/assets/loading.png");
-  },
-  create: function() {
+
+
+class Boot extends Phaser.State {
+	init(){
+		game.scale.forceOrientation(false, true);
+	}
+	
+	preload(){
+		this.game.load.image("loading","game/assets/loading.png");
+	}
+	create(){
 	  console.log("boot state");
 	  this.game.state.start("Preload");
-  },
-  update: function() {
-  }
-};
+		
+	}
+}
